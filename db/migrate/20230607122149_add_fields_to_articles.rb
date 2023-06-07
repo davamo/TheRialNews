f7aa266cd3beb665d7c9c6c16f3acd9ab4617ffc
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-06-07 09:10:19 -0400

    Feat: create models article, comment and migrates

---
+++ db/migrate/20230607122149_add_fields_to_articles.rb
@@ -0,0 +1,7 @@
+class AddFieldsToArticles < ActiveRecord::Migration[7.0]
+  #def change
+  #  add_column :articles, :title, :string
+  #  add_column :articles, :content, :text
+  #  add_reference :articles, :user, null: false, foreign_key: true
+  #end
+end
