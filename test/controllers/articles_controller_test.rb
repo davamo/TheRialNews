156852f2cfb158f31dde62a56b65a8a474a08696
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-06-10 10:55:42 -0400

    feat: modified test articles

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,32 @@
-require "test_helper"
+require 'test_helper'
+class ArticlesControllerTest < ActionController::TestCase
+  def setup
+    @article = articles(:one) # Asumiendo que tienes un artículo existente en tu base de datos de pruebas
+  end
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:articles)
+  end
+
+  test "should show article" do
+    get :show, params: { id: @article.id }
+    assert_response :success
+    assert_not_nil assigns(:article)
+  end
+
+  test "should redirect to login when creating article without authentication" do
+    post :create, params: { article: { title: "Test", content: "Lorem ipsum" } }
+    assert_redirected_to login_path
+  end
+
+  test "should redirect to root when non-admin user tries to create article" do
+    sign_in users(:non_admin_user) # Asumiendo que tienes un usuario no administrador en tus datos de prueba
+    post :create, params: { article: { title: "Test", content: "Lorem ipsum" } }
+    assert_redirected_to root_path
+  end
+
+  # More tests for other controller actions...
+
 end
